73dd998fa783f26ef8d67cbe7fd4fba586cf57c7
doniyoryuldashev95 <doniyoryuldashev95@gmail.com>
2022-09-04 02:50:18 +0500

    s

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double x = (Double.valueOf(toBeDivided))/5;
+        System.out.println(x);
     }
 }
